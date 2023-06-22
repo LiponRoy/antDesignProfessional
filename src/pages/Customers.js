3fa9ee54abfeb0e15b4584d81757e21c59c0fb55
sakhawat777 <liponbdbd@gmail.com>
2023-06-22 16:55:43 +0600

    ant design Table prac

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import { Table, Tag } from "antd";
+import React, { useEffect, useState } from "react";
 
 const Customers = () => {
-  return (
-    <div className=' h-screen flex justify-center items-center px-10'>
-      
-      <span className=' text-pink-800 text-4xl'>Customar </span>
+  const [columnsAl, setColumnsAll] = useState([
+    {
+      title:'ID',
+      dataIndex: '_id',
+    },
+    {
+      title: 'Image',
+      dataIndex: 'image',
+      width: 50,
+      maxWidth: 50,
+      render: (t, r) => <img src={`${r.image.url}`} />
+    },
+    {
+      title:'Name',
+      dataIndex: 'name',
+    },
     
-    </div>
-  )
-}
+    {
+      title: 'Brand',
+      dataIndex: 'brand',
+      
+    },
+    {
+      title:'Price',
+      dataIndex: 'price',
+    },
+  ]);
 
-export default Customers
+  const [allData, setAllData] = useState([]);
+  useEffect(() => {
+    fetch("https://liponroy-lotus-e-shop-backend-api-2023.onrender.com/api/product/getAll")
+      .then((res) => res.json())
+      .then((result) => {
+        setAllData(result);
+      });
+      console.log(allData)
+  },[]);
+  return <div>
+    <Table columns={columnsAl} dataSource={allData}></Table>
+  </div>;
+};
+
+export default Customers;
