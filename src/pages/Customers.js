042df7701c2d248a0ab17bf79b7717acb3acccc4
Lipon Roy <liponbdbd@gmail.com>
2023-06-24 08:29:14 +0600

    add mongodb Data to antd table

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,47 +1,67 @@
-import { Table, Tag } from "antd";
-import React, { useEffect, useState } from "react";
+import { Button, Table, Tag } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 
 const Customers = () => {
-  const [columnsAl, setColumnsAll] = useState([
-    {
-      title:'ID',
-      dataIndex: '_id',
-    },
-    {
-      title: 'Image',
-      dataIndex: 'image',
-      width: 50,
-      maxWidth: 50,
-      render: (t, r) => <img src={`${r.image.url}`} />
-    },
-    {
-      title:'Name',
-      dataIndex: 'name',
-    },
-    
-    {
-      title: 'Brand',
-      dataIndex: 'brand',
-      
-    },
-    {
-      title:'Price',
-      dataIndex: 'price',
-    },
-  ]);
+	// store all data here
+	const [allData, setAllData] = useState([]);
+	// Add Edit Modal Visibility useState
+	const [addEditModalVisibality, seAddEditModalVisibality] = useState(false);
+	// columns for table
+	const [columnsAl, setColumnsAll] = useState([
+		{
+			title: 'Name',
+			dataIndex: 'name',
+		},
+		{
+			title: 'Image',
+			dataIndex: 'image',
+			// width: 120,
+			// maxWidth: 120,
+			render: (t, r) => <img src={`${r.image}`} height='80' width='80' />,
+		},
+		{
+			title: 'Phone',
+			dataIndex: 'phone',
+		},
+		{
+			title: 'Gender',
+			dataIndex: 'gender',
+		},
+		{
+			title: 'Gender',
+			dataIndex: 'gender',
+			render: (id, record) => (
+				<div className=' flex justify-center items-center gap-x-2'>
+					<DeleteOutlined className=' mx-2 text-lg cursor-pointer' />
+					<EditOutlined className=' mx-2 text-lg cursor-pointer' />
+				</div>
+			),
+		},
+	]);
 
-  const [allData, setAllData] = useState([]);
-  useEffect(() => {
-    fetch("https://liponroy-lotus-e-shop-backend-api-2023.onrender.com/api/product/getAll")
-      .then((res) => res.json())
-      .then((result) => {
-        setAllData(result);
-      });
-      console.log(allData)
-  },[]);
-  return <div>
-    <Table columns={columnsAl} dataSource={allData}></Table>
-  </div>;
+	const dataFetch = () => {
+		fetch('http://localhost:4000/user/allUser')
+			.then((res) => res.json())
+			.then((result) => {
+				setAllData(result);
+			});
+	};
+
+	useEffect(() => {
+		dataFetch();
+	}, []);
+
+	return (
+		<div>
+			<div className=' flex justify-between items-center px-[.10rem] py-4'>
+				<div className=' text-lg'>Total Item :{allData.length}</div>
+				<Button className=' bg-zinc-600 text-white'>ADD USER</Button>
+			</div>
+
+			<Table columns={columnsAl} dataSource={allData}></Table>
+		</div>
+	);
 };
 
 export default Customers;
